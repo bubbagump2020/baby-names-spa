607b3fdb531b199c09a36f69ef724956a9c04ab9
bubbagump2020 <bubbagump2020@gmail.com>
2020-07-07 16:32:10 -0500

    Working on Netlify Lambda functions

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom'
 import { post } from 'axios'
 import randomstring from 'randomstring'
 import { ROOT_URL } from './Constants'
-import { handler } from '../functions/babies-index'
+// import { handler } from '../functions/babies-index'
 
 const SplashPage = () => {
 
@@ -20,7 +20,7 @@ const SplashPage = () => {
             }
             // let response = null
 
-            const response = handler()
+            // const response = handler()
             // console.log(response)
 
             // try {
