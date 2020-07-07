47c14e3ea2301e037b18abed6b6917a6ba959c00
bubbagump2020 <bubbagump2020@gmail.com>
2020-07-07 17:02:40 -0500

    Working on Netlify Lambda functions

--- src/components/functions/babies-index.js
+++ src/components/functions/babies-index.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
-const ROOT_URL = 'baby-maker-2000-api.com'
+const ROOT_URL = 'https://baby-maker-2000-api.com'
 
-exports.handler = async (event, context, callback) => {
+const handler = exports.handler = async (event, context, callback) => {
     try{
         const response = await axios.get(`${ROOT_URL}/babies`)
         const data = response.data
