89c4ec771e9c7a34e1c3c2c383cd75c9b6bfb36f
Kevin <bubbagump2020@gmail.com>
2020-07-07 12:11:38 -0500

    Updated SplashPage axios url with api url for testing. All other requests still point to localhost.

---
+++ baby_names_spa/src/components/Constants.js
@@ -0,0 +1,1 @@
+export const ROOT_URL = 'baby-maker-2000-api.com'
--- baby_names_spa/src/components/SplashPage.jsx
+++ baby_names_spa/src/components/SplashPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom'
 import { post } from 'axios'
 import randomstring from 'randomstring'
+import { ROOT_URL } from './Constants'
 
 const SplashPage = () => {
 
@@ -17,8 +18,9 @@ const SplashPage = () => {
                 })
             }
             let response = null
+
             try {
-                response = await post('http://localhost:3001/lists', request)
+                response = await post(`${ROOT_URL}/lists`, request)
                 if (response.status === 200) {
                     unique_id = response.data.list.unique_id;
                     history.push(`/lists/${unique_id}`);
@@ -26,6 +28,22 @@ const SplashPage = () => {
             } catch {
 
             }
+            // const response = await fetch(`/lists`,{
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify({
+            //         unique_id: randomstring.generate({
+            //             length: 12,
+            //             charset: 'alphanumeric'
+            //         })
+            //     })
+            // })
+
+            // const data = await response.json()
+            // console.log(data)
+
         }
         makeList();
     });
