50bff83ae44eeeb097c9908319b65cb8da23a32b
bubbagump2020 <bubbagump2020@gmail.com>
2020-07-08 17:24:06 -0500

    Connected, test client query working, switching to pool query for scalability

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,16 +1,15 @@
 const axios = require("axios")
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
 const ROOT_URL = 'https://baby-maker-2000-api.herokapp.com'
 // const ROOT_URL = 'http://localhost:3001'
 const query = 'INSERT INTO babies(list_id, baby_name) VALUES($1, $2) RETURNING'
-const client = new Client({
+const pool = new Pool({
     connectionString: "postgres://lpqbtrivtlrque:82902c27b34536fbf4c2db63aa18e3a591a154d770080c51988209927472ccab@ec2-34-192-173-173.compute-1.amazonaws.com:5432/d2tqs2vejh2i12",
     ssl: {
         rejectUnauthorized: false
     }
 })
-console.log('beginning client connection')
-client.connect()
+// client.connect()
 
 exports.handler = (event, context) => {
     const form = JSON.parse(event.body)
@@ -22,34 +21,21 @@ exports.handler = (event, context) => {
     };
     let babyResponse;
     try {
-        console.log(babyRequest)
-        client.query("SELECT * FROM babies", (error, response) => {
-            console.log(error)
-            console.log(response)
-            console.log('ending client connection')
-            client.end()
+        // Working Client Query
+        // client.query("SELECT * FROM babies", (error, response) => {
+        //     console.log(error)
+        //     console.log(response)
+        //     client.end()
+        // })
+        pool.query("SELECT * FROM babies", (error, response) => {
+            if(error){
+                throw error
+            }
+            babyResponse = response
         })
-        console.log('ending client connection')
-        // client.end()
-        // axios.post(`${ROOT_URL}/babies`, babyRequest)
-        //     .then(function(response ){
-        //         console.log(response)
-        //     })
-        //     .catch(function(error) {
-        //         console.log(error)
-        //     })
-        // xhr.open('POST', `${ROOT_URL}/babies`)
-        // xhr.setRequestHeader('Content-Type', 'application/json')
-        // xhr.send(JSON.stringify(form))
-        // xhr.onload = function(e) {
-        //     if (xhr.readyState === 4){
-        //         if (xhr.status === 200){
-        //             response = JSON.parse(xhr.responseText)
-        //         } else {
-        //             console.log(xhr.statusText)
-        //         }
-        //     }
-        // }
+
+        console.log(babyResponse)
+
         return{
             statusCode: 200,
             body: JSON.stringify(babyResponse)
