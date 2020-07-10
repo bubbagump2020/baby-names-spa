8d069f40a71e018e2c66b606e10a0ff8305afe8b
bubbagump2020 <bubbagump2020@gmail.com>
2020-07-09 21:42:22 -0500

    hunting for deployment bugs

--- src/components/BabyNameForm.jsx
+++ src/components/BabyNameForm.jsx
@@ -129,8 +129,7 @@ const BabyNameForm = () => {
                         <h1>The Baby Maker 2000</h1>
                         <p>Simply put in a name and it'll be saved!</p>
                         <p>Note: To return to this list save your URL some where safe</p>
-                        <Form onSubmit={handleSubmit} method="post" name="baby">
-                            <input type="hidden" name="form-name" value="bab"/>
+                        <Form onSubmit={handleSubmit}>
                             <Form.Group>
                                 <Form.Label>Name!</Form.Label>
                                 <Form.Control type="text" placeholder="Baby Name" name="baby-name" onChange={e => handleChange(e)}/>
