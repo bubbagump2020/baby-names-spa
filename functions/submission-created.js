a9f6ffb44a1d7488b6db741bff7feb4af9471fb3
bubbagump2020 <bubbagump2020@gmail.com>
2020-07-10 12:22:20 -0500

    Hunting down last bugs

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 const { Pool } = require('pg')
-// const ROOT_URL = 'https://baby-maker-2000-api.herokapp.com'
-const ROOT_URL = 'http://localhost:3001'
+const ROOT_URL = 'https://baby-maker-2000-api.herokapp.com'
+// const ROOT_URL = 'http://localhost:3001'
 
 const pool = new Pool({
     connectionString: "postgres://lpqbtrivtlrque:82902c27b34536fbf4c2db63aa18e3a591a154d770080c51988209927472ccab@ec2-34-192-173-173.compute-1.amazonaws.com:5432/d2tqs2vejh2i12",
