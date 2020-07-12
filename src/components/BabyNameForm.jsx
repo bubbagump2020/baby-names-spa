347caae1c8d1f0270a426924b9762dbdf58b8e2f
bubbagump2020 <bubbagump2020@gmail.com>
2020-07-12 15:15:11 -0500

    404 message maybe squashed?

--- src/components/BabyNameForm.jsx
+++ src/components/BabyNameForm.jsx
@@ -144,7 +144,7 @@ const BabyNameForm = () => {
                         <h1>The Baby Maker 2000</h1>
                         <p>Simply put in a name and it'll be saved!</p>
                         <p>Note: To return to this list save your URL some where safe</p>
-                        <form id="form" onSubmit={handleSubmit}>
+                        <form id="form" data-netlify="true" method="post" name="baby" onSubmit={handleSubmit}>
                             <input type="hidden" name="form-name" value="baby" />
                             <div>
                                 <label>Name! </label>
